df2680e703d6198c8f2ebae1ebb6a81f0c6a5d02
Tudor Grigoriu <gregorescu@outlook.com>
2021-10-17 18:08:21 +0300

    added base row model

--- app/models/table-row.js
+++ app/models/table-row.js
@@ -1,15 +1,16 @@
 import { tracked } from '@glimmer/tracking';
+import BaseRowModel from '../components/cool-table/models/base-row-model';
 
-export const AvailableStatus = "available";
+export const AvailableStatus = 'available';
 
-export default class TableRow {
-  @tracked isSelected = false;
+export default class TableRow extends BaseRowModel {
   name; //'7za.exe',
   device; //'Baratheon',
   path; //'\\Device\\HarddiskVolume1\\temp\\7za.exe',
   status; //'scheduled',
 
   constructor(args) {
+    super(args);
     this.name = args.name;
     this.device = args.device;
     this.path = args.path;
